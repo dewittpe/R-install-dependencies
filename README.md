4f6ed1ea3dd81591526fb58f345f6a90cd32e5b7
Peter DeWitt <dewittpe@gmail.com>
2018-02-24 23:02:20 -0700

    initial commit for github version of this work.

---
+++ README.md
@@ -0,0 +1,39 @@
+# R Install Dependencies
+
+Consider the following situation.  You need to install a set of R packages on a
+machine that cannot make external http(s) requests.   As such, you must download
+source files for the pacakge, along with all of the dependencies, and
+dependencies of dependencies, etc, and transfer these source files to the
+machine (likely via FTP) that cannot make the external http(s) requests.
+Further, you need to install the packages in the correct order so that the
+install of a dependency does not fail do to a missing dependencies of the the
+dependency.
+
+The scrpt `build-dep-list.R` will generate the needed list of dependencies,
+download the source files, from both [CRAN](https://cran.r-project.org) and
+[BioConductor](https://www.bioconductor.org/), and then generate a `makefile`
+to install the packages in an order so that R package dependencies should not
+cause errors.
+
+## Use
+
+On a computer, with internet access run the following script
+
+    Rscript --vanilla build-dep-list.R pkg1 [pkg2] [pkg3] [...] [pkgn]
+
+The R script will download the source (.tar.gz) file for pkg1 and the
+dependencies (packages listed under Depends, Imports, and LinksTo in the
+DESCRIPTION file) along with the dependencies of the dependencies, and so on.
+You may list multiple packages here and the n packages and dependencies will be
+downloaded.
+
+The source files will be placed in a `pkg-source-files` directory.
+
+The R script will generate a `makefile`
+
+After transfering the `makefile` and the `pkg-source-files` directory to
+the remote machine, the user need only to run [GNU make](https://www.gnu.org/software/make/)
+the needed packages.
+
+    make
+
---
+++ build-dep-list.R
@@ -0,0 +1,94 @@
+################################################################################
+# file: build-dep-list.R
+#
+# This script is used to get the source (.tar.gz) files for R packages and
+# recursive dependencies so that the source files can be transfered to, and
+# installed on, machines without internet access.
+#
+# The result of this script will place source files (.tar.gz) for the packages,
+# and the package dependencies, and dependencies of dependencies, and so on, in
+# the pkg-source-files directory.
+#
+# A makefile will be generated to install the packages.
+#
+# Moving the pkg-source-files directory, and the makefile via FTP
+# to the target machine should be sufficient for installing on that machine,
+# assuming that R and GNU make have been successfully installed on that machine.
+#
+# This script is expected to be evaluated from the command line via:
+#  
+#   Rscript --vanilla build-dep-list.R [pkg1] [pkg2] [...] [pkgn]
+#
+# Where pkg1 is the name of the first known package to download, pkg2 the second
+# known package to download, ..., and pkgn the nth package to download.  The
+# script will download all the dependencies for pkg1, ..., pkgn, and the
+# dependencies of the dependencies, and so on.
+################################################################################
+
+# For testing and development, use a subset of packages.  If this script is
+# called as noted above then the command line args will be used.
+if (interactive()) {
+  OUR_PACKAGES <- c("graph", "gRbase", "gRain", "jsonlite", "plotly", "SHELF",
+                    "rjson", "svglite", "magrittr")
+} else {
+  OUR_PACKAGES <- commandArgs(trailingOnly = TRUE)
+}
+
+# Repositories to look for packages
+CRAN <- "https://cran.rstudio.com/"
+BIOC <- "https://bioconductor.org/packages/release/bioc/"
+
+# Define the base packages.  These are packages which come with R upon install
+# of R.  These packages include: "base", "compiler", "datasets", "graphics",
+# "grDevices", "grid", "methods", "parallel", "splines", "stats", "stats4",
+# "tcltk", "tools", and "utils".
+#
+# NOTE: there are Priority = "recommended" packages as well.  If these packages
+# are missing from the system install, this script might fail.  Downloading and
+# installing the 'recommended' packages can be difficult between R versions.
+base_pkgs <-
+  unname(utils::installed.packages()[utils::installed.packages()[, "Priority"] %in% c("base", "recommended"), "Package"])
+
+# get a list of the available packages from CRAN and BioConductor
+available_pkgs <- available.packages(repos = c(CRAN, BIOC))
+
+# use the tools::package_dependencies function to generate a list of the
+# packages dependencies, and dependencies of dependencies, and so on, ...
+pkgs_to_download <- OUR_PACKAGES
+i <- 1L
+while(i <= length(pkgs_to_download)) {
+  deps <-
+    unlist(tools::package_dependencies(packages = pkgs_to_download[i],
+                                       which = c("Depends", "Imports", "LinkingTo"),
+                                       db = available_pkgs,
+                                       recursive = FALSE),
+           use.names = FALSE) 
+  deps <- deps[!(deps %in% base_pkgs)]
+  pkgs_to_download <- append(pkgs_to_download, deps, i) 
+  i <- i + 1L
+}
+pkgs_to_download <- unique(rev(pkgs_to_download))
+
+# Download the needed packages into the pkg-source-files directory
+unlink("pkg-source-files/*")
+dir.create("pkg-source-files/", showWarnings = FALSE)
+
+dwnld_pkgs <-
+  download.packages(pkgs = pkgs_to_download,
+                    destdir = "pkg-source-files",
+                    repos = c(CRAN, BIOC),
+                    type = "source")
+
+# generate a makefile to install the packages.  The makefile will stop if there
+# is an error in any of the installs.  Using a bash script will not stop if
+# there is an error.
+
+cat("all:\n",
+    paste0("\tR CMD INSTALL ", dwnld_pkgs[, 2], "\n"),
+    sep = "", 
+    file = "makefile")
+
+
+################################################################################
+#  end of file 
+################################################################################
